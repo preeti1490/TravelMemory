a69e3b5a539b344765767e15a3c4000db4db0a82
preeti1490 <106378281+preeti1490@users.noreply.github.com>
2025-09-07 12:59:25 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/aryanm12/TravelMemory.git'
+            }
+        }
+        stage('Install') {
+            steps {
+                sh 'cd backend; npm install'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'cd backend; npm run build'
+            }
+        }
+    }
+
+}
+
+
+
